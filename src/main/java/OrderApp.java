f07698db74f3e924ed9ab5e84ea1552ecaa704cd
Meotjh <mallangti@naver.com>
2022-04-01 17:55:12 +0900

    Initial commit

---
+++ src/main/java/OrderApp.java
@@ -0,0 +1,20 @@
+import hello.core.member.*;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1l;
+        Member member = new Member( memberId, "memberA", Grade.VIP );
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+    }
+}
---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    //원래는 동시성 이슈로 인해 concurrenthashmap을 써야하지만
+    //예제기 때문에HashMap으로 사용
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(long memberId) {
+        return store.get(memberId);
+    }
+}
